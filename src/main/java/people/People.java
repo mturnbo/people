e253f2af913983db9e177757841dbccb65ce80b7
Marcus E. Turnbo <marcus@emarcus.com>
2015-08-23 17:45:23 -0500

    Added SpringBootServletInitializer

--- src/main/java/people/People.java
+++ src/main/java/people/People.java
@@ -2,15 +2,22 @@ package people;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
- *  People application
+ *  People API Application
  *
  *  @author Marcus Turnbo
  */
 
 @SpringBootApplication
-public class People {
+public class People extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(People.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(People.class, args);
--- src/main/java/people/repository/PersonRepository.java
+++ src/main/java/people/repository/PersonRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
- *  Person object
+ *  Person respository
  *
  *  @author Marcus Turnbo
  */
