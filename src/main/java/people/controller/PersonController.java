e3351d5e47b2ac68e95105672b6c689d7952e530
Marcus E. Turnbo <marcus@emarcus.com>
2015-08-24 11:54:47 -0500

    Update Person controller - changed response body to Person object and checked for null objects.

--- src/main/java/people/controller/PersonController.java
+++ src/main/java/people/controller/PersonController.java
@@ -3,8 +3,10 @@ package people.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.http.HttpServletResponse;
 import people.model.Family;
 import people.model.Person;
 import people.repository.PersonRepositoryCrud;
@@ -20,10 +22,16 @@ public class PersonController {
     private FamilyRepositoryCrud familyRepository;
 
     @RequestMapping(value = "/updateFamily", method = RequestMethod.POST)
-    public String updateFamily(@RequestParam("person") Long personId, @RequestParam("family") Long familyId) {
+    @ResponseBody
+    public Person updateFamily(@RequestParam("person") Long personId, @RequestParam("family") Long familyId,
+                               HttpServletResponse response) {
         Person person = personRepository.findOne(personId);
         Family family = familyRepository.findOne(familyId);
-        person.setFamily(family);
-        return "success";
+        if (person != null && family != null) {
+            person.setFamily(family);
+        } else {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+        return person;
     }
 }
